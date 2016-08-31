912cc284fc3bd30d43dd97517e5e11bd6df3002d
Anirudh Sivaraman <sk.anirudh@gmail.com>
2016-08-31 17:17:31 -0400

    Add remaining two examples from HotNets

---
+++ example_queries/route_reconvergence.sql
@@ -0,0 +1,5 @@
+def path_change([last_path, ch_count], [pkt_path]):
+  if pkt_path != last_path then ch_count = ch_count + 1
+  last_path = pkt_path
+
+result = SELECT [srcip, dstip, srcport, dstport, proto, path_change] GROUPBY [srcip, dstip, srcport, dstport, proto] FROM T;
