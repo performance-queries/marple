6b6393a2e9c92b7a24731962c7fbe4961c956301
Srinivas Narayana <ngsrinivas@gmail.com>
2016-09-01 14:57:47 -0400

    flowlet sum example

---
+++ example_queries/flowlet_sum.sql
@@ -0,0 +1,15 @@
+def fl_detect ([last_time, size], [tin]):
+    delta = 5;
+    if tin - last_time > delta then {
+        emit;
+	size = 1 }
+    else {
+        size = size + 1; }
+    last_time = tin
+
+def flsum ([sum], [size]):
+    sum = sum + size
+
+R1 = SELECT fl_detect FROM T GROUPBY [srcip, dstip, srcport, dstport,
+         proto];
+result = SELECT flsum from R1 GROUPBY [];
