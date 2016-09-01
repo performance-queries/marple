bb1665020a4a9ea6c64195bf94f48d3e06e06a8f
Srinivas Narayana <ngsrinivas@gmail.com>
2016-09-01 15:25:26 -0400

    add flowlet histogram example

---
+++ example_queries/flowlet_hist.sql
@@ -0,0 +1,16 @@
+def fl_detect ([last_time, size], [tin]):
+    delta = 5;
+    if tin - last_time > delta then {
+        emit;
+	size = 1 }
+    else {
+        size = size + 1; }
+    last_time = tin
+
+def flsum ([sum], [size]):
+    sum = sum + size
+
+R1 = SELECT fl_detect FROM T GROUPBY [srcip, dstip, srcport, dstport,
+         proto];
+R2 = SELECT [size/100] FROM R1 as [size_index];
+result = SELECT flsum from R2 GROUPBY [ size_index ];
