021a4dee66282e24c099dbc1814e1ca8bffb27f0
Srinivas Narayana <ngsrinivas@gmail.com>
2016-09-01 16:18:55 -0400

    incast

---
+++ example_queries/incast.sql
@@ -0,0 +1,12 @@
+def new_flow ([count], []):
+    if count == 0 then { emit; count = 1 };
+
+def flow_count ([count], []):
+    count = count + 1
+
+R1 = select [srcip, dstip, srcport, dstport, proto, tin/128] from T as
+     [srcip, dstip, srcport, dstport, proto, epoch];
+R2 = select new_flow from R1 groupby [srcip, dstip, srcport, dstport, proto, epoch];
+R3 = select flow_count from R2 groupby [];
+R4 = R3 JOIN T;
+result = select * from R4 where qin > 100 && flow_count > 50;
