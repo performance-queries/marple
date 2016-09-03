f212698d06be654878d472a39aa10e2966b0ead0
Anirudh Sivaraman <sk.anirudh@gmail.com>
2016-09-03 16:21:33 -0400

    Can't use an aggregate function name as column
    First error that we fixed because of the compiler's error reporting.

--- example_queries/incast.sql
+++ example_queries/incast.sql
@@ -1,8 +1,8 @@
 def new_flow ([count], []):
     if count == 0 then { emit; count = 1 };
 
-def flow_count ([count], []):
-    count = count + 1
+def flow_count ([f_count], []):
+    f_count = f_count + 1
     emit
 
 R1 = select [srcip, dstip, srcport, dstport, proto, tin/128] from T as
@@ -10,4 +10,4 @@ R1 = select [srcip, dstip, srcport, dstport, proto, tin/128] from T as
 R2 = select new_flow from R1 sgroupby [srcip, dstip, srcport, dstport, proto, epoch];
 R3 = select flow_count from R2 sgroupby [epoch];
 R4 = R3 JOIN T;
-result = select * from R4 where qin > 100 && flow_count > 50;
+result = select * from R4 where qin > 100 && f_count > 50;
