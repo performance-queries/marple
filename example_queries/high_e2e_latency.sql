1f17c31dae2098a0ff50f3acf077f299e28eff3b
Anirudh Sivaraman <sk.anirudh@gmail.com>
2016-09-06 21:55:33 -0400

    Per NG, make sure we track the switch ID as well
    Makes the query more intuitive

--- example_queries/high_e2e_latency.sql
+++ example_queries/high_e2e_latency.sql
@@ -1,10 +1,11 @@
-def e2esum([e2e_latency], [tout, tin]):
-  e2e_latency = e2e_latency + (tout - tin)
+def e2esum([e2e_latency, last_switch], [tout, tin, switch_id]):
+  e2e_latency = e2e_latency + (tout - tin);
+  last_switch = switch_id;
   emit;
 
 def empty([], []):
   ;
 
 R1 = SELECT e2esum FROM T SGROUPBY [uid];
-R3 = SELECT * FROM R1 WHERE e2e_latency > L;
+R3 = SELECT * FROM R1 WHERE e2e_latency > L && last_switch == egress_switch;
 R4 = SELECT empty FROM R3 RGROUPBY [srcip, dstip, srcport, dstport, proto];
