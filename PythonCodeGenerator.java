bfe6483828b0bfcd5ad66599ce15a63bbd06df49
Anirudh Sivaraman <sk.anirudh@gmail.com>
2016-09-08 17:18:18 -0400

    Test harness, with random inputs for intrinsic fields alone (qid, qin, ...)

--- PythonCodeGenerator.java
+++ PythonCodeGenerator.java
@@ -5,17 +5,26 @@ import org.antlr.v4.runtime.tree.TerminalNode;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.misc.Interval;
 import java.util.List;
+import java.util.TreeSet;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.lang.RuntimeException;
 
 public class PythonCodeGenerator extends perf_queryBaseListener {
+  /// Intrinsic fields understood by the compiler
+  private final TreeSet<String> intrinsic_fields_ = new TreeSet<String>(Arrays.asList("qid", "qin", "tin", "tout", "pkt_path", "switch_id", "uid"));
+
   /// Reference to parser to get underlying token stream.
   /// Required to preserve spaces and retrieve them when unparsing productions
   private perf_queryParser parser_;
 
-  /// A reference to the symbol table created by the SymbolTableCreator pass
-  private HashMap<String, IdentifierType> symbol_table_;
+  /// All fields in tuples,
+  /// start with intrinsic fields alone
+  private TreeSet<String> tuple_field_set_ = new TreeSet<String>(intrinsic_fields_);
+
+  /// All fields in state
+  private TreeSet<String> state_field_set_ = new TreeSet<String>();
 
   /// Build up function calls string
   private String function_calls_ = "";
@@ -23,9 +32,24 @@ public class PythonCodeGenerator extends perf_queryBaseListener {
   /// Build up function definitions string
   private String function_defs_ = "";
 
+  /// Last assigned stream
+  private String last_assigned_ = "";
+
   public PythonCodeGenerator(perf_queryParser t_parser, HashMap<String, IdentifierType> t_symbol_table) {
     parser_ = t_parser;
-    symbol_table_ = t_symbol_table;
+    
+    for (String key : t_symbol_table.keySet()) {
+      if (t_symbol_table.get(key) == IdentifierType.COLUMN) {
+        tuple_field_set_.add(key);
+      }
+    }
+    tuple_field_set_.add("valid");
+
+    for (String key : t_symbol_table.keySet()) {
+      if (t_symbol_table.get(key) == IdentifierType.STATE) {
+        state_field_set_.add(key);
+      }
+    }
   }
 
   /// Use this to print declarations at the beginning of Python code
@@ -33,6 +57,16 @@ public class PythonCodeGenerator extends perf_queryBaseListener {
   @Override public void enterProg(perf_queryParser.ProgContext ctx) {
     System.err.println(generate_state_class());
     System.err.println(generate_tuple_class());
+    System.err.println("def fuse(x, y):");
+    System.err.println("  if (x is None) : return y;");
+    System.err.println("  if (y is None) : return x;");
+    System.err.println("  raise Exception (\"Can't fuse, both x and y are not None\")");
+    System.err.println("def random_tuple():");
+    System.err.println("  ret = Tuple();");
+    for (String key : intrinsic_fields_) {
+      System.err.println("  " + "ret." + key + " = random.randint(1, 65536);\n");
+    }
+    System.err.println("  return ret;");
   }
 
   /// Use this to print the packet loop that tests the given sql program
@@ -40,7 +74,14 @@ public class PythonCodeGenerator extends perf_queryBaseListener {
   @Override public void exitProg(perf_queryParser.ProgContext ctx) {
     System.err.println(function_defs_);
     System.err.println("# main loop of function calls");
+    System.err.println("state_dict = dict();");
+    System.err.println("for i in range(1000):");
+    System.err.println("  T = random_tuple(); # generates a random tuple");
+    System.err.println("  print \"input:\", T;");
+    System.err.println("  T.print_tuple();");
     System.err.println(function_calls_);
+    System.err.println("  print \"output:\"," + last_assigned_ + ";");
+    System.err.println("  " + last_assigned_ + ".print_tuple();");
   }
 
   /// Turn aggregation function into a Python function definitoon
@@ -91,7 +132,7 @@ public class PythonCodeGenerator extends perf_queryBaseListener {
     return (spg_query_signature(stream) +
             generate_tuple_preamble() + "\n" +
             "  valid = " + text_with_spaces(predicate) + "\n\n" +
-            generate_tuple_postamble());
+            generate_tuple_postamble() + "\n");
   }
 
   /// Turn SQL projections into Python function definitions
@@ -101,15 +142,16 @@ public class PythonCodeGenerator extends perf_queryBaseListener {
     return (spg_query_signature(stream) +
             generate_tuple_preamble() + "\n" +
             "  " + text_with_spaces(col_list) + " = " + text_with_spaces(expr_list) + ";\n\n" +
-            generate_tuple_postamble());
-  } 
+            generate_tuple_postamble()+ "\n");
+  }
 
   /// Turn SQL joins into Python function definitions
   private String join_def(ParseTree query, ParseTree stream) {  
     return (join_query_signature(stream) +
             "  ret_tuple = Tuple();\n" +
+            "  ret_tuple = tuple1.join_tuple(tuple2);\n" +
             "  ret_tuple.valid = tuple1.valid and tuple2.valid\n" +
-            "  return ret;\n");
+            "  return ret;\n" + "\n");
   }
 
   /// Turn SQL GROUPBYs into Python function definitions
@@ -121,8 +163,7 @@ public class PythonCodeGenerator extends perf_queryBaseListener {
             generate_tuple_preamble() + "\n" +
             "  key_for_dict = tuple(" + text_with_spaces(groupby_list) + ");\n\n" +
             "  tuple_state = state_dict[key_for_dict] if key_for_dict in state_dict else State();\n\n" +
-            "  return " + text_with_spaces(agg_func) + "(tuple_state, tuple_var);" + "\n");
-            // TODO: Support different values of initial state
+            "  return " + text_with_spaces(agg_func) + "(tuple_state, tuple_var);" + "\n" + "\n");
   }
 
   @Override public void exitStream_stmt(perf_queryParser.Stream_stmtContext ctx) {
@@ -132,6 +173,7 @@ public class PythonCodeGenerator extends perf_queryBaseListener {
     ParseTree query = ctx.getChild(2);
     assert(query instanceof perf_queryParser.Stream_queryContext);
 
+    last_assigned_ = text_with_spaces((ParserRuleContext)stream);
     OperationType operation = getOperationType((perf_queryParser.Stream_queryContext)query);
     if (operation == OperationType.FILTER) {
       function_defs_  += filter_def(query, stream);
@@ -157,6 +199,7 @@ public class PythonCodeGenerator extends perf_queryBaseListener {
     ParseTree query = ctx.getChild(2);
     assert(query instanceof perf_queryParser.Relational_queryContext);
 
+    last_assigned_ = text_with_spaces((ParserRuleContext)stream);
     OperationType operation = getOperationType((perf_queryParser.Relational_queryContext)query);
     if (operation == OperationType.RFOLD) {
       function_defs_  += groupby_def(query, stream);
@@ -182,7 +225,7 @@ public class PythonCodeGenerator extends perf_queryBaseListener {
   private String generate_spg_queries(ParseTree query, ParseTree stream) {
     String stream_name = text_with_spaces((ParserRuleContext)stream);
     String arg_name    = text_with_spaces((ParserRuleContext)query.getChild(0).getChild(3));
-    return stream_name + " = func" + stream_name + "(" + arg_name + ")\n";
+    return "  " + stream_name + " = func" + stream_name + "(" + arg_name + ")\n";
   }
 
   /// Generate Python function call for join queries
@@ -190,7 +233,7 @@ public class PythonCodeGenerator extends perf_queryBaseListener {
     String stream_name = text_with_spaces((ParserRuleContext)stream);
     String arg1   = text_with_spaces((ParserRuleContext)query.getChild(0).getChild(0));
     String arg2   = text_with_spaces((ParserRuleContext)query.getChild(0).getChild(2));
-    return stream_name + " = func" + stream_name + "(" + arg1 + ", " + arg2 + ")\n";
+    return "  " + stream_name + " = func" + stream_name + "(" + arg1 + ", " + arg2 + ")\n";
   }
 
   private String text_with_spaces(ParserRuleContext production) {
@@ -200,57 +243,70 @@ public class PythonCodeGenerator extends perf_queryBaseListener {
   }
 
   private String generate_tuple_class() {
-    String ret = "# tuple class\n";
+    String ret = "import random;\n";
+    ret += "random.seed(1)\n";
+    ret += "# tuple class\n";
     ret += "class Tuple: \n";
     ret += "  def __init__(self):\n";
-    for (String key : symbol_table_.keySet()) {
-      if (symbol_table_.get(key) == IdentifierType.COLUMN) {
-        ret = ret + "    " + "self." + key + " = 0;\n";
-      }
+    for (String key : tuple_field_set_) {
+      ret = ret + "    " + "self." + key + " = None;\n";
     }
     ret += "    self.state = State();\n";
-    ret += "    self.valid = False;\n";
+    ret += "    self.valid = True;\n"; // Everything is valid in the base stream
+
+    ret += "  def print_tuple(self):\n";
+    for (String key : tuple_field_set_) {
+      ret += "    print \"" + key + "\"," + "self." + key + "\n";
+    }
+    ret += "    self.state.print_state();\n";
+
+    ret += "  def join_tuple(self, other):\n";
+    ret += "    ret = Tuple();\n";
+    for (String key : tuple_field_set_) {
+      ret += "    ret." + key + " = fuse(self." + key + ", other." + key + ");\n";
+    }
+    ret += "    return ret;\n";
+
     return ret;
   }
 
   private String generate_tuple_preamble() {
     String ret = "  # tuple preamble\n";
-    for (String key : symbol_table_.keySet()) {
-      if (symbol_table_.get(key) == IdentifierType.COLUMN) {
-        ret = ret + "  " + key + " = tuple_var." + key + "\n";
-      }
+    for (String key : tuple_field_set_) {
+      ret = ret + "  " + key + " = tuple_var." + key + "\n";
     }
     return ret;
   }
 
   private String generate_tuple_postamble() {
     String ret = "  # tuple postamble\n";
-    for (String key : symbol_table_.keySet()) {
-      if (symbol_table_.get(key) == IdentifierType.COLUMN) {
-        ret = ret + "  " + "tuple_var." + key + " = " + key + "\n";
-      }
+    for (String key : tuple_field_set_) {
+      ret = ret + "  " + "tuple_var." + key + " = " + key + "\n";
     }
-    return ret + "\n" + "  return tuple_var;\n";
+    ret += "  return tuple_var;\n";
+    return ret;
   }
 
   private String generate_state_class() {
     String ret = "# state class\n";
     ret += "class State: \n";
     ret += "  def __init__(self):\n";
-    for (String key : symbol_table_.keySet()) {
-      if (symbol_table_.get(key) == IdentifierType.STATE) {
-        ret = ret + "    " + "self." + key + " = 0;\n";
-      }
+    // For now, we are assuming all state is init. to 0
+    for (String key : state_field_set_) {
+      ret = ret + "    " + "self." + key + " = 0;\n";
     }
+    ret += "  def print_state(self):\n";
+    for (String key : state_field_set_) {
+      ret = ret + "    print \"" + key + "\", self." + key +";\n";
+    }
+
     return ret;
   }
 
   private String emit_stub() {
     String ret = "";
-    for (String key : symbol_table_.keySet()) {
-      if (symbol_table_.get(key) == IdentifierType.STATE) {
-        ret = ret + key.substring(3) + " = " + key + ";";
-      }
+    for (String key : state_field_set_) {
+      ret = ret + key.substring(3) + " = " + key + ";";
     }
     ret += "# emit stub";
     return ret;
@@ -258,26 +314,21 @@ public class PythonCodeGenerator extends perf_queryBaseListener {
 
   private String generate_state_preamble() {
     String ret = "  # state preamble\n";
-    for (String key : symbol_table_.keySet()) {
-      if (symbol_table_.get(key) == IdentifierType.STATE) {
-        ret = ret + "  " + key + " = state." + key + "\n";
-      }
+    for (String key : state_field_set_) {
+      ret = ret + "  " + key + " = state." + key + "\n";
     }
     return ret;
   }
 
   private String generate_state_postamble() {
     String ret = "  # state postamble\n";
-    for (String key : symbol_table_.keySet()) {
-      if (symbol_table_.get(key) == IdentifierType.STATE) {
-        ret = ret + "  " + "state." + key + " = " + key + "\n";
-      }
+    for (String key : state_field_set_) {
+      ret = ret + "  " + "state." + key + " = " + key + "\n";
     }
     return ret;
   }
 
   /// Get operation type for the given query
-  /// TODO: This duplicates a lot of code with ExprTreeCreator
   private OperationType getOperationType(ParserRuleContext query) {
     assert(query instanceof perf_queryParser.Stream_queryContext ||
            query instanceof perf_queryParser.Relational_queryContext);
