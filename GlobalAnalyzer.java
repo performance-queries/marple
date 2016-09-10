b1a836a7565446321d7e27b0ac23e64c04486ae0
Srinivas Narayana <ngsrinivas@gmail.com>
2016-09-10 17:32:23 -0400

    Global analyzer now uses predicate switch extractor class for filters.

--- GlobalAnalyzer.java
+++ GlobalAnalyzer.java
@@ -4,53 +4,30 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.lang.RuntimeException;
 
 /// Determine which queries run where, and (conceptually) which set of packets
 /// each instance of the query processes. This results in an annotated query
 /// expression tree.
 public class GlobalAnalyzer extends perf_queryBaseVisitor<Void> {
-    /// A reference to the symbol table created by the SymbolTableCreator pass
-    private HashMap<String, IdentifierType> symbol_table_;
+    /// A reference to the set of switches globally in the network
+    private HashSet<Integer> all_switches_;
 
     /// Internal "value expression" detection instance
-    private ValueExprDetector value_detect_;
+    private SwitchPredicateExtractor switch_extractor_;
 
     /// Constructor
-    public GlobalAnalyzer(HashMap<String, IdentifierType> symbol_table) {
-	symbol_table_ = symbol_table;
-	value_detect_ = new ValueExprDetector();
-    }
-
-    /// Call visitor methods for the "value detection" class on the current
-    /// expression.
-    private Void valueDetect(perf_queryParser.ExprContext ctx) {
-	System.out.println("Got an expression!");
-	System.out.println(ctx.getText());
-	System.out.println(value_detect_.visit(ctx).toString());
-	return null;
+    public GlobalAnalyzer(HashSet<Integer> all_switches) {
+	all_switches_ = all_switches;
+	switch_extractor_ = new SwitchPredicateExtractor(all_switches_);
     }
 
     /// Test methods to check out some basic functionalities
     @Override public Void visitFilter(perf_queryParser.FilterContext ctx) {
-	System.out.println(ctx.predicate().getText());
+	HashSet<Integer> affected_sw = switch_extractor_.visit(ctx.pred());
+	System.out.println("This filter restricts the stream to the following switches:");
+	System.out.println(affected_sw.toString());
 	return null;
     }
-
-    @Override public Void visitExprCol(perf_queryParser.ExprColContext ctx) {
-    	return valueDetect(ctx);
-    }
-
-    @Override public Void visitExprInf(perf_queryParser.ExprInfContext ctx) {
-    	return valueDetect(ctx);
-    }
-
-    @Override public Void visitExprVal(perf_queryParser.ExprValContext ctx) {
-    	return valueDetect(ctx);
-    }
-
-    @Override public Void visitExprComb(perf_queryParser.ExprCombContext ctx) {
-    	return valueDetect(ctx);
-    }
-
 }
