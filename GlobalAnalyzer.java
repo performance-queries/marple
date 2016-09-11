457ddd93264b7ed38f540cf7f97f8996cd40e2b5
Srinivas Narayana <ngsrinivas@gmail.com>
2016-09-10 22:03:48 -0400

    Add recursive operator-level OpLocation generation. Fleshed out for filters.

--- GlobalAnalyzer.java
+++ GlobalAnalyzer.java
@@ -17,20 +17,68 @@ public class GlobalAnalyzer extends perf_queryBaseVisitor<OpLocation> {
     /// Internal "value expression" detection instance
     private SwitchPredicateExtractor switch_extractor_;
 
+    /// A reference to the symbol to query parse tree from previous passes.
+    private HashMap<String, ParserRuleContext> sym_tree_;
+
+    /// A reference to the last assigned ID in the query program.
+    private String last_assigned_id_;
+
     /// Constructor
-    public GlobalAnalyzer(HashSet<Integer> all_switches) {
+    public GlobalAnalyzer(HashSet<Integer> all_switches,
+			  HashMap<String, ParserRuleContext> sym_tree,
+			  String last_assigned_id) {
 	all_switches_ = all_switches;
 	switch_extractor_ = new SwitchPredicateExtractor(all_switches_);
+	sym_tree_ = sym_tree;
+	last_assigned_id_ = last_assigned_id;
+    }
+
+    /// Return OpLocation from underlying operators, which are inputs to the current operator.
+    private OpLocation RecurseDeps(String stream) {
+	/// Recursively get OpLocation information for the operand streams.
+	/// But first, ensure that the input stream has been seen before. It's an assert instead of
+	/// exception because the previous pass should have caught the error.
+	OpLocation opl_inputs;
+	if (! stream.equals("T")) {
+	    ParserRuleContext subquery = sym_tree_.get(stream);
+	    assert(subquery != null);
+	    opl_inputs = visit(subquery);
+	} else {
+	    opl_inputs = new OpLocation();
+	}
+	return opl_inputs;
     }
 
     /// Test methods to check out some basic functionalities
     @Override public OpLocation visitFilter(perf_queryParser.FilterContext ctx) {
 	HashSet<Integer> sw_set = switch_extractor_.visit(ctx.pred());
-	OpLocation filter_opl = new OpLocation(predicated_sw, StreamType.SINGLE_SWITCH_STREAM);
-	System.out.println("This filter restricts the stream to the following switches:");
-	System.out.println(sw_set.toString());
-	/* TODO: Recursive call to input stream/relation's OpLocation, and merging of the two
-	OpLocations. */
-	return filter_opl;
+	OpLocation opl_inputs = RecurseDeps(ctx.stream().getText());
+	/// Merge values from recursive call and current
+	sw_set.retainAll(opl_inputs.getSwitchSet());
+	if (opl_inputs.getStreamType() == StreamType.SINGLE_SWITCH_STREAM) {
+	    return new OpLocation(sw_set, StreamType.SINGLE_SWITCH_STREAM);
+	} else if (sw_set.size() > 1) {
+	    return new OpLocation(sw_set, StreamType.MULTI_SWITCH_STREAM);
+	} else {
+	    return new OpLocation(sw_set, StreamType.SINGLE_SWITCH_STREAM);
+	}
+    }
+
+    @Override public OpLocation visitProject(perf_queryParser.ProjectContext ctx) {
+	return RecurseDeps(ctx.stream().getText());
+    }
+
+    @Override public OpLocation visitRfold(perf_queryParser.RfoldContext ctx) {
+	return RecurseDeps(ctx.stream().getText());
+    }
+
+    @Override public OpLocation visitJoin(perf_queryParser.JoinContext ctx) {
+	return new OpLocation();
+    }
+
+    @Override public OpLocation visitProg(perf_queryParser.ProgContext ctx) {
+	ParserRuleContext subquery = sym_tree_.get(last_assigned_id_);
+	assert (subquery != null);
+	return visit(subquery);
     }
 }
