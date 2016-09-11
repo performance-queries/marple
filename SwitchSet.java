5ec6d4a9da7c99349963612ac8093002d7fa711e
Srinivas Narayana <ngsrinivas@gmail.com>
2016-09-10 22:02:49 -0400

    Network-level switch configuration completely in SwitchSet class.

--- SwitchSet.java
+++ SwitchSet.java
@@ -2,15 +2,20 @@ import java.util.HashSet;
 
 /// Create a set representing the set of switch identifiers in the network.
 public class SwitchSet {
+    /// The set of all switch identifiers in the network.
     private HashSet<Integer> all_switches_ = new HashSet<Integer>();
 
-    public SwitchSet(int net_size) {
-	for (int i=0; i < net_size; i++) {
+    /// An integer denoting the number of switches.
+    private Integer switch_count_ = 20;
+
+    /// A temporary construction for the set of network switch identifiers.
+    public SwitchSet() {
+	for (int i=0; i < switch_count_; i++) {
 	    all_switches_.add(i);
 	}
     }
 
-    public HashSet<Integer> get_switches() {
+    public HashSet<Integer> getSwitches() {
 	return all_switches_;
     }
 }
