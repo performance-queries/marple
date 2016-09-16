f44813d6c63b850b85b0a75bcae227448587f036
Anirudh Sivaraman <sk.anirudh@gmail.com>
2016-09-16 08:52:31 -0400

    Replace class name with JAR in test_everything.sh

--- test_everything.sh
+++ test_everything.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 set -e;
 if [ $# -ne 1 ]; then
-  echo "Usage: ./test_everything.sh module"; 
+  echo "Usage: ./test_everything.sh jar_file";
   exit
 fi
 
 for f in example_queries/*.sql; do
   echo "Testing $f ...";
-  cat $f | java -ea $1 ;
+  cat $f | java -ea -jar $1 ;
 done 
