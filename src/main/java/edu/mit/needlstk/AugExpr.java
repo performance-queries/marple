db3ca1ed76be6ea89c52eaa4930cf91f47129312
Srinivas Narayana <ngsrinivas@gmail.com>
2016-11-20 20:30:01 -0500

    Augmented expression implementation

---
+++ src/main/java/edu/mit/needlstk/AugExpr.java
@@ -0,0 +1,93 @@
+package edu.mit.needlstk;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ParseTree;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AugExpr {
+  public enum AugExprType {
+    EXPR_ID,
+    EXPR_VAL,
+    EXPR_COMB
+  };
+
+  public enum Binop {
+    BINOP_ADD,
+    BINOP_SUB,
+    BINOP_MUL,
+    BINOP_DIV
+  };
+
+  /// Enum and operator identifying the structure of the expression tree
+  public AugExprType type;
+
+  /// If the expression is just an identifier:
+  public String ident;
+
+  /// If the expression is a value: an integer or -1 (for infinity). There are only positive
+  /// integers in the grammar as of now.
+  public Integer value;
+
+  /// Children expressions and combinator if this is a compound expression
+  public List<AugExpr> children;
+  public Binop binop;
+
+  /// Constructor from expression in ANTLR (ExprContext)
+  public AugExpr(PerfQueryParser.ExprContext ctx) {
+    if (PerfQueryParser.ExprColContext.isInstance(ctx)) {
+      this.type = EXPR_ID;
+      this.ident = ctx.ID().getText();
+    } else if (PerfQueryParser.ExprValContext.isInstance(ctx)) {
+      this.type = EXPR_VAL;
+      String valText = ctx.VALUE().getText();
+      this.value = (valText.equals("infinity")) ? -1 : Integer.valueOf(valText);
+    } else if (PerfQueryParser.ExprCombContext.isInstance(ctx)) {
+      this.type = EXPR_COMB;
+      this.children = MakeExprChildren(new AugExpr(ctx.expr(0)), new AugExpr(ctx.expr(1)));
+      this.binop = binopFromText(ctx.op.getText());
+    } else if (PerfQueryParser.ExprParenContext.isInstance(ctx)) {
+      copy(new AugExpr(ctx.expr()));
+    } else {
+      assert(false); // Logic error. Expecting a different kind of expression?
+    }
+  }
+
+  /// Constructor for specific case of expression with a supplied identifier
+  public AugExpr(String id) {
+    this.type = EXPR_ID;
+    this.ident = id;
+  }
+
+  /// Helper to get binary operator enum from token text
+  private Binop binopFromText(String txt) {
+    switch(txt) {
+      case "+":
+        return Binop.BINOP_ADD;
+      case "-":
+        return Binop.BINOP_SUB;
+      case "*":
+        return Binop.BINOP_MUL;
+      case "/":
+        return Binop.BINOP_DIV;
+      default:
+        assert (false); // Expecting a different expression combinator?
+    }
+  }
+
+  private void copy(AugExpr copySrc) {
+    this.type = copySrc.type;
+    this.ident = copySrc.ident;
+    this.value = copySrc.value;
+    this.children = copySrc.children;
+    this.binop = copySrc.binop;
+  }
+
+  /// Helper to construct children list of AugExprs from two input arguments
+  private List<AugExpr> MakeExprChildren(AugExpr childLeft, AugExpr childRight) {
+    List<AugExpr> children = new ArrayList<AugExpr>();
+    children.add(childLeft);
+    children.add(childRight);
+    return children;
+  }
+}
