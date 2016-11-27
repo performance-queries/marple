404f3efece30de7f7f30a813f57013ea69e81dc9
Srinivas Narayana <ngsrinivas@gmail.com>
2016-11-27 13:26:53 -0500

    Check define before use for  three-op-statements

--- src/main/java/edu/mit/needlstk/DefineBeforeUse.java
+++ src/main/java/edu/mit/needlstk/DefineBeforeUse.java
@@ -18,6 +18,7 @@ public class DefineBeforeUse {
     this.code = code;
     this.stateVars = stateVars;
     this.fieldVars = fieldVars;
+    this.symTable = new HashMap<String, HashMap<String, AggFunVarType>>();
   }
 
   public boolean check() {
@@ -27,13 +28,44 @@ public class DefineBeforeUse {
       assert(stateVars.containsKey(aggFun));
       assert(fieldVars.containsKey(aggFun));
       ThreeOpCode toc = code.get(aggFun);
+      symTable.put(aggFun, new HashMap<String, AggFunVarType>());
+      initSymTable(aggFun);
       checkToc(aggFun, toc);
     }
     return true;
   }
 
-  public boolean checkToc(String aggFun, ThreeOpCode toc) {
+  private void initSymTable(String aggFun) {
     /// Bootstrap the agg-fun-specific symbol table with formal parameters
+    for (String state: stateVars.get(aggFun)) {
+      symTable.get(aggFun).put(state, AggFunVarType.STATE);
+    }
+    for (String field: fieldVars.get(aggFun)) {
+      symTable.get(aggFun).put(field, AggFunVarType.FIELD);
+    }
+  }
+
+  public boolean checkToc(String aggFun, ThreeOpCode toc) {
+    /// Go through function body to check define before use
+    for (ThreeOpStmt stmt: toc.stmts) {
+      for(String usedVar: stmt.getUsedVars()) {
+        /// Check if each is available in symbol table
+        if(! symTable.get(aggFun).containsKey(usedVar)) {
+          /// TODO: Error condition. Flag the error to user.
+          throw new RuntimeException("Using variable " + usedVar + " in function " +
+                                     aggFun + " without prior definition");
+        }
+      }
+      String definedVar = stmt.getDefinedVar();
+      /// TODO: Add to symbol table if not a field var already in the symbol table
+      if(symTable.get(aggFun).containsKey(definedVar) &&
+         symTable.get(aggFun).get(definedVar) == AggFunVarType.FIELD) {
+        throw new RuntimeException("Can't set a packet field" + definedVar
+                                   + " directly in function " + aggFun);
+      } else if(! symTable.get(aggFun).containsKey(definedVar)) {
+        symTable.get(aggFun).put(definedVar, AggFunVarType.FN_VAR);
+      }
+    }
     return true;
   }
   
