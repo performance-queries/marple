aab380706256c9766d5104264832fbb394cf877f
Srinivas Narayana <ngsrinivas@gmail.com>
2016-11-27 13:25:58 -0500

    Refine types for agg fun variables

--- src/main/java/edu/mit/needlstk/AggFunVarType.java
+++ src/main/java/edu/mit/needlstk/AggFunVarType.java
@@ -1,7 +1,8 @@
 package edu.mit.needlstk;
 
 public enum AggFunVarType {
-  STATE, // state of the aggregation function
-  FIELD, // packet field, per-packet arguments to the function
-  FNVAR  // internal temporary function variables
+  STATE,    // state of the aggregation function
+  FIELD,    // packet field, per-packet arguments to the function
+  FN_VAR,   // internal temporary function variables
+  PRED_VAR  // predicate variable, defined in code transformation
 };
