a3f8e59e83968c6c090195e43b8d0cc168c7a812
Srinivas Narayana <ngsrinivas@gmail.com>
2016-12-07 16:29:18 -0500

    Implementation of lexically scoped symbol tables to catch use-before-define errors

---
+++ src/main/java/edu/mit/needlstk/LexicalSymbolTable.java
@@ -0,0 +1,102 @@
+package edu.mit.needlstk;
+import java.util.HashMap;
+import java.util.List;
+
+public class LexicalSymbolTable extends PerfQueryBaseVisitor<Boolean> {
+  /// A member that always represents the symbol table for the current context
+  private HashMap<String, AggFunVarType> outerSymTable;
+
+  /// A member that always represents the agg fun for the current context
+  private String currAggFun;
+
+  /// A map from aggregation functions to their list of state arguments
+  private HashMap<String, List<String>> stateVars;
+
+  /// A map from aggregation functions to their list of field arguments
+  private HashMap<String, List<String>> fieldVars;
+
+  /// Constructor
+  public LexicalSymbolTable(HashMap<String, List<String>> stateVars,
+                            HashMap<String, List<String>> fieldVars) {
+    this.stateVars = stateVars;
+    this.fieldVars = fieldVars;
+  }
+
+  /// Initialize symbol table for an aggregation function with state and field formal arguments
+  private void initSymTable(String aggFun, HashMap<String, AggFunVarType> symTable) {
+    /// Bootstrap the agg-fun-specific symbol table with formal parameters
+    for (String state: stateVars.get(aggFun)) {
+      symTable.put(state, AggFunVarType.STATE);
+    }
+    for (String field: fieldVars.get(aggFun)) {
+      symTable.put(field, AggFunVarType.FIELD);
+    }
+  }
+
+  /// ANTLR visitor for `primitive.` Using the symbol table for the current lexical context,
+  /// determine whether expressions use variables that are not yet defined.
+  @Override public Boolean visitPrimitive(PerfQueryParser.PrimitiveContext ctx) {
+    if (ctx.ID() != null) {
+      AugExpr assignedExpr = new AugExpr(ctx.expr());
+      for (String usedVar: assignedExpr.getUsedVars()) {
+        if (! outerSymTable.containsKey(usedVar)) {
+          throw new RuntimeException("Using variable " + usedVar + " in function " +
+                                     currAggFun + " without prior definition\n" +
+                                     ctx.getText());
+        }
+      }
+      String definedVar = ctx.ID().getText();
+      assert (definedVar != null);
+      if (! outerSymTable.containsKey(definedVar)) {
+        outerSymTable.put(definedVar, AggFunVarType.FN_VAR);
+      } else if (outerSymTable.get(definedVar) == AggFunVarType.FIELD) {
+          throw new RuntimeException("Can't set a packet field" + definedVar
+                                     + " directly in function " + currAggFun);
+      } // Nothing more to be done if it's already defined to be a local variable.
+    } // Nothing else to be done for emit and ; primitive statements
+    return true;
+  }
+
+  /// ANTLR visitor for predicate. Use the symbol table to check if all variables used in the
+  /// predicate have been defined earlier.
+  /// Note: This has been defined not as the `predicate` visitor because of the sub-casing of ANTLR
+  /// rules, which would require a separate visitor method for each production from `predicate`.
+  public Boolean checkPred(PerfQueryParser.PredicateContext ctx) {
+    AugPred pred = new AugPred(ctx);
+    for (String usedVar: pred.getUsedVars()) {
+      if (! outerSymTable.containsKey(usedVar)) {
+        throw new RuntimeException("Using variable " + usedVar + " in function " +
+                                   currAggFun + " without prior definition\n" +
+                                   ctx.getText());
+      }
+    }
+    return true;
+  }
+
+  @Override public Boolean visitIfConstruct(PerfQueryParser.IfConstructContext ctx) {
+    // start a new symbol table for the new lexical context
+    HashMap<String, AggFunVarType> oldOuterSymTable = new HashMap<>(outerSymTable);
+    // check the predicate, if branch, and else branch.
+    checkPred(ctx.predicate());
+    for (PerfQueryParser.IfPrimitiveContext ifCtx: ctx.ifPrimitive()) {
+      visit(ifCtx);
+    }
+    for (PerfQueryParser.ElsePrimitiveContext elseCtx: ctx.elsePrimitive()) {
+      visit(elseCtx);
+    }
+    outerSymTable = oldOuterSymTable;
+    return true;
+  }
+
+  @Override public Boolean visitAggFun(PerfQueryParser.AggFunContext ctx) {
+    // Initialize an outer symbol table using stateVars and fieldVars
+    currAggFun = ctx.aggFunc().getText();
+    outerSymTable = new HashMap<String, AggFunVarType>();
+    initSymTable(currAggFun, outerSymTable);
+    // Check define-before-use for every statement in the function
+    for (PerfQueryParser.StmtContext stmt: ctx.stmt()) {
+      visit(stmt);
+    }
+    return true;
+  }
+}
