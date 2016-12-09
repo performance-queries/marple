c47b5cf0f30f09157701266438d6faab0a2f4807
Srinivas Narayana <ngsrinivas@gmail.com>
2016-12-08 20:19:15 -0500

    Fix use before define error in example

--- example_queries/flowlet_hist.sql
+++ example_queries/flowlet_hist.sql
@@ -4,8 +4,8 @@ def fl_detect ([last_time, size], [tin]):
     else { size = size + 1; }
     last_time = tin
 
-def flsum ([sum], [size]):
-    sum = sum + size
+def flsum ([sum], [size_index]):
+    sum = sum + size_index
 
 R1 = groupby(T, [srcip, dstip, srcport, dstport, proto, switch], fl_detect);
 R2 = map(R1, [size_index], [size/100]);
