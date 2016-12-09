e6f2ea3969dde0d1773b9b20b5222c6e6fa367d5
Srinivas Narayana <ngsrinivas@gmail.com>
2016-12-08 20:17:25 -0500

    Add new fields from performance schema to the packet log

--- src/main/java/edu/mit/needlstk/Fields.java
+++ src/main/java/edu/mit/needlstk/Fields.java
@@ -16,6 +16,9 @@ public class Fields {
   public static String payloadlenHdr = "len";
   public static String pktlenHdr = "len";
   public static String qidHdr = "qid";
+  public static String tinHdr = "tin";
+  public static String toutHdr = "tout";
+  public static String pktpathHdr = "pktpath";
 
   public static ArrayList<String> fields = new ArrayList<>(Arrays.asList(
       switchHdr,
@@ -29,5 +32,8 @@ public class Fields {
       outportHdr,
       payloadlenHdr,
       pktlenHdr,
-      qidHdr));
+      qidHdr,
+      tinHdr,
+      toutHdr,
+      pktpathHdr));
 }
