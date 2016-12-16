2760c6b41c24bd194a141580c2e11b6b856490a7
Srinivas Narayana <ngsrinivas@gmail.com>
2016-12-16 13:23:41 -0500

    Fix bug in flowlet histogram query

--- example_queries/flowlet_hist.sql
+++ example_queries/flowlet_hist.sql
@@ -4,8 +4,8 @@ def fl_detect ([last_time, size], [tin]):
     else { size = size + 1; }
     last_time = tin
 
-def flsum ([sum], [size_index]):
-    sum = sum + size_index
+def flsum ([sum], []):
+    sum = sum + 1
 
 R1 = groupby(T, [srcip, dstip, srcport, dstport, proto, switch], fl_detect);
 R2 = map(R1, [size_index], [size/100]);
