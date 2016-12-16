1dd7d1d8c643aa470fbcf0c526b0232de8164f3b
Srinivas Narayana <ngsrinivas@gmail.com>
2016-12-16 00:12:36 -0500

    Streamline fields into headers and standard metadata, with corresponding P4 field names

--- src/main/java/edu/mit/needlstk/Fields.java
+++ src/main/java/edu/mit/needlstk/Fields.java
@@ -1,6 +1,7 @@
 package edu.mit.needlstk;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 /// List of pre-defined fields in the global packet log
 public class Fields {
@@ -22,8 +23,18 @@ public class Fields {
   public static String pktpathHdr = "pktpath";
   public static String qlenHdr = "qlen";
 
-  public static ArrayList<String> fields = new ArrayList<>(Arrays.asList(
+  public static ArrayList<String> metadataFields = new ArrayList<>(Arrays.asList(
       switchHdr,
+      inportHdr,
+      outportHdr,
+      pktlenHdr,
+      payloadlenHdr,
+      qidHdr,
+      tinHdr,
+      toutHdr,
+      qlenHdr));
+
+  public static ArrayList<String> headerFields = new ArrayList<>(Arrays.asList(
       uidHdr,
       srcipHdr,
       dstipHdr,
@@ -31,13 +42,35 @@ public class Fields {
       dstportHdr,
       tcpseqHdr,
       protoHdr,
-      inportHdr,
-      outportHdr,
-      payloadlenHdr,
-      pktlenHdr,
-      qidHdr,
-      tinHdr,
-      toutHdr,
-      pktpathHdr,
-      qlenHdr));
+      pktpathHdr));
+
+  public static final ArrayList<String> fields = new ArrayList<>();
+  static {
+    fields.addAll(metadataFields);
+    fields.addAll(headerFields);
+  }
+
+  /// Map from query field name to p4 field names
+  public static HashMap<String, String> p4Map = new HashMap<>();
+  static {
+    /// Metadata fields
+    p4Map.put(switchHdr,     "switch");            // TODO: corresponding p4 field?
+    p4Map.put(inportHdr,     "ingress_port");
+    p4Map.put(outportHdr,    "egress_port");
+    p4Map.put(pktlenHdr,     "packet_length");
+    p4Map.put(payloadlenHdr, "payload_length");    // TODO: corresponding p4 field?
+    p4Map.put(qidHdr,        "qid");               // TODO: corresponding p4 field?
+    p4Map.put(tinHdr,        "ingress_timestamp"); // TODO: corresponding p4 field?
+    p4Map.put(toutHdr,       "egress_timestamp");  // TODO: corresponding p4 field?
+    p4Map.put(qlenHdr,       "queue_len");         // TODO: corresponding p4 field?
+    /// Header fields
+    p4Map.put(uidHdr,        "p.ip.identification");
+    p4Map.put(srcipHdr,      "p.ip.srcAddr");
+    p4Map.put(dstipHdr,      "p.ip.dstAddr");
+    p4Map.put(srcportHdr,    "p.tcp.srcport");     // TODO: parse tcp srcport
+    p4Map.put(dstportHdr,    "p.tcp.dstport");     // TODO: parse tcp dstport
+    p4Map.put(tcpseqHdr,     "p.tcp.sequence");    // TODO: parse tcp sequence number
+    p4Map.put(protoHdr,      "p.ip.protocol");
+    p4Map.put(pktpathHdr,    "p.pktpath");         // TODO: add a pktpath header?
+  }
 }
