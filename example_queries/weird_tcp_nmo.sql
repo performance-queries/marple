14553e1d942a95ee87859109fc9ade78e9ef1269
Srinivas Narayana <ngsrinivas@gmail.com>
2016-12-28 11:52:11 -0500

    Example to test nested if-else construct

---
+++ example_queries/weird_tcp_nmo.sql
@@ -0,0 +1,8 @@
+def nonmt([maxseq, nm_count], [tcpseq]):
+  if maxseq > tcpseq { nm_count = nm_count + 1; }
+  else {
+    if maxseq < tcpseq { maxseq = tcpseq; }
+  }
+
+tcp_pkts = filter(T, proto == 6);
+nmo_query = groupby(tcp_pkts, [srcip, dstip, srcport, switch, dstport, proto], nonmt);
