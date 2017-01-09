44752d71833fa77b08a9a99a23873d8914138aff
Srinivas Narayana <ngsrinivas@gmail.com>
2017-01-09 17:33:52 -0500

    Automate testing outputs with domino

---
+++ domino_paths.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Provide domino binary and atom paths here.
+export DOMINO_BIN="/Users/ngsrinivas/Desktop/research/domino/domino-compiler/domino";
+export DOMINO_ATOMS="/Users/ngsrinivas/Desktop/research/domino/domino-examples/banzai_atoms/";
--- test_everything.sh
+++ test_everything.sh
@@ -5,7 +5,15 @@ if [ $# -ne 1 ]; then
   exit
 fi
 
+# Hack: Domino binary and atom paths are defined in domino_paths.sh.
+source domino_paths.sh;
+
 for f in example_queries/*.sql; do
   echo "Testing $f ...";
   cat $f | java -ea -jar $1 ;
-done 
+  # Testing with domino
+  $DOMINO_BIN domino-full.c ${DOMINO_ATOMS}/nested_ifs.sk 16 10 ;
+done
+
+echo ;
+echo "No smoking guns.";
